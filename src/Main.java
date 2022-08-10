add6fd175c06bac4e2f4b4a1793a856061129398
Ragnazar <ivan.avdeyev@wilix.org>
2022-08-10 21:03:31 +0300

    add-basics

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
+    public static Employee[] employees;
+
     public static void main(String[] args) {
+        employees = new Employee[3];
+        employees[0] = new Employee("Ivanov Ivan Ivanovich", 1, 10000);
+        employees[1] = new Employee("Sidorov Ivan Petrovich", 2, 12000);
+        employees[2] = new Employee("Petrov Ivan Andreevich", 5, 22000);
+
+        for (Employee e : employees) {
+            System.out.println(e.toString());
+        }
+
+        System.out.println(Employee.getSum(employees));
+        System.out.println(Employee.averageSalary(employees));
 
 
+
+
+        }
     }
-}
+
+
+
+
